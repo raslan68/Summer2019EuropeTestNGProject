5c6e7a193e0350aef4b24a02e622e7c3af484cb6
Ramazan Aslan <raslan68@yahoo.com>
2019-11-04 11:52:37 +0100

    day3 text cases

--- src/test/java/com/cybertek/tests/day3_webelemt_intro/VerifyURLnotchanged.java
+++ src/test/java/com/cybertek/tests/day3_webelemt_intro/VerifyURLnotchanged.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-public class VerifyURLnotChamged {
+public class VerifyURLnotchanged {
 
     /**
      * verify URL not changed
